21de9159618267535eb9fcdecdec7bc7010ad707
Mario Quintana <pyrros2000@gmail.com>
2018-09-01 13:34:30 -0700

    Adding files to setp a simple server but getting error about missing middleware

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const http = require('http');
 const app = require('./lib/app');
 const connect = require('./lib/util/connect');
 
-connect(process.env. MONGODB_URI);
+connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/disco-dog');
 
 const server = http.createServer(app);
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-    console.log('Server on', PORT);
+    console.log('Server running on', PORT);
 });
