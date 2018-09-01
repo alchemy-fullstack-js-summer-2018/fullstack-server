4037181b03f17660f483e8210ef5ca98bb220465
Mario Quintana <pyrros2000@gmail.com>
2018-09-01 14:34:39 -0700

    Server now running have error message about mongoose

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const http = require('http');
 const app = require('./lib/app');
 const connect = require('./lib/util/connect');
 
-connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/disco-dog');
+connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/disco-dogs');
 
 const server = http.createServer(app);
-const PORT = process.env.PORT || 3000;
+const port = process.env.port || 3000;
 
-server.listen(PORT, () => {
-    console.log('Server running on', PORT);
+server.listen(port, () => {
+    // eslint-disable-next-line
+    console.log('Server Running On', server.address().port);
 });
